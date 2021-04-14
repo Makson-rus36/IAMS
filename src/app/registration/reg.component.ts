e7ae40b461bc5676d7567df24e669101e017b6c7
Maks <maksim.t00@gmail.com>
2021-04-14 20:40:45 +0300

    Дизайн регистрации готов

--- src/app/registration/reg.component.ts
+++ src/app/registration/reg.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Page} from '@nativescript/core';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-reg',
@@ -8,9 +9,24 @@ import {Page} from '@nativescript/core';
 })
 
 export class RegComponent implements OnInit {
-    constructor(private page: Page) {
+    nameUser = '';
+    lastNameUser = '';
+    otchUser = '';
+    phoneNumberUser = '';
+    emailUser = '';
+    passwordUser = '';
+
+    constructor(private page: Page, private router: Router) {
     }
     ngOnInit() {
         this.page.actionBarHidden = true;
+    }
+
+    backAuth($event) {
+        this.router.navigate(['enter']);
+    }
+
+    createAcc($event) {
+        this.router.navigate(['home']);
     }
 }
